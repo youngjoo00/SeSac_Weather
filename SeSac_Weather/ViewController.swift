7e1bf8edd8ea33683f5afcf2390d843d365efb18
youngjoo00 <yungjoo0624@gmail.com>
2024-02-11 11:46:46 +0900

    add: 프로젝트 추가

---
+++ SeSac_Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSac_Weather
+//
+//  Created by youngjoo on 2/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
